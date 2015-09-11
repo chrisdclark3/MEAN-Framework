3b47aac50c14a7fc705ad96e5ec60d391ad88d8d
Christopher Clark <chrisdclark3@gmail.com>
2015-09-10 23:33:33 -0700

    first

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ var server = require('http').createServer(app);
     server.listen(port);
 
 app.use(express.static('./app/client'));
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host       : 'localhost',
+  socketPath : '/tmp/mysql.sock',
+  port       : '3306',
+  user       : 'root',
+  password   : '',
+  debug      : true,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log("\n\n\nERROR!!!!");
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+});
